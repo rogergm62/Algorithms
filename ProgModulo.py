0358e2c8ee43808e92ad0c4da10b78cbe86d1363
rogelio <rogergm62@msn.com>
2024-06-25 17:59:54 -0700

    Congruence Modulo Algorithm

---
+++ ProgModulo.py
@@ -0,0 +1,23 @@
+# This program will find the Modulo. Python has a function for this algorith %
+# This program reproduces the same result.
+
+def modulo(a, n):
+    # This part finds the quotient of the division. Just returns the integer part.
+    # Perform the division where a is the dividend and n is the divisor
+    result = a / n
+    
+    # Extract just the integer part
+    quotient = int(result)
+        
+    # Calculate the modulo (remainder r = a - nq )   
+    product = quotient * n
+    remainder = a - product
+    
+    return remainder    
+        
+    
+print ("a " + '\u2261' + " b (mod n)")
+n= int(input("n? "))
+a= int(input ("a? "))
+print
+print (f"{a} (mod {n}) \u2261 {modulo(a,n)}")
